8b41f17aff11fabddbdf1ce172678c1cda220b9f
huy <yuandong.hu@booxware.de>
2017-06-20 17:11:04 +0200

    spring boot application main class and build.gradle

---
+++ import-service/import-service-app/src/main/java/com/wewash/services/ImportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wewash.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ImportServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImportServiceApplication.class);
+    }
+
+}
